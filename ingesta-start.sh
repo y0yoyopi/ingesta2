419cd20551b65ef49a036c999ce964eedcfa47a6
y0yoyopi <165915247+y0yoyopi@users.noreply.github.com>
2025-05-22 18:32:31 -0500

    Add files via upload

---
+++ ingesta-start.sh
@@ -0,0 +1,5 @@
+python3 ingesta01.py
+python3 ingesta02.py
+python3 ingesta03.py
+python3 ingesta04.py
+python3 ingesta05.py
---
+++ ingesta04.py
@@ -0,0 +1,7 @@
+from utec_logger.logger import logger
+
+logger.info("Server started successfully.")
+logger.warning("Invalid configuration detected.")
+logger.error("Failed to connect to the database.")
+logger.critical("Unexpected system error occurred. Shutting down.")
+logger.info("Data processing completed successfully.")
